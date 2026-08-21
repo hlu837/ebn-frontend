bb1d46b558629989963ac720010929dd1f369cee
hlu837 <hilinayared438@gmail.com>
2026-08-21 15:27:22 -0700

    Fix AppUser import in login_screen and imageUrl initialization in _CategoryTile

--- lib/screens/role_gate_screen.dart
+++ lib/screens/role_gate_screen.dart
@@ -1057,6 +1057,7 @@ class _CategoryTile extends StatelessWidget {
   const _CategoryTile({
     required this.label,
     required this.icon,
+    this.imageUrl,
     required this.selected,
     required this.onTap,
   });
